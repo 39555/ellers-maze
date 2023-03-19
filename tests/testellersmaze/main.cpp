e61aa3f32377c13b01949e5e7d93ed8c852ddc00
autogalkin <galigorrom@gmail.com>
2023-03-19 16:57:05 +0400

    a stable version but is  a little bit not optimised

--- tests/testellersmaze/main.cpp
+++ tests/testellersmaze/main.cpp
@@ -1,6 +1,7 @@
 #include <__ranges/join_view.h>
 #include <iostream>
 #include <iterator>
+#include <string>
 #include <vector>
 #include "ellers-maze.h"
 #include "unistd.h"
@@ -19,19 +20,45 @@ void test_line_type_switching(){
 
 void test_print_lines()
 {
-    int width = 50;
-    auto gen = ellersmaze(width);
+    // | 1 | 2 | 3 | 4 |
+    //  ___ ___ ___ ___
+    // ^~ 
+    int width = 30;
+    auto m = maze(width);
+    //auto gen = ellersmaze(width);
     while(true) {
-       line l =  gen();
-       line l2 = gen();
-    
+       auto l  = m.gen_v_line();
+       auto setv = m.cells_and_its_set; 
+       auto l2 = m.gen_h_line();
+       auto seth = m.cells_and_its_set;
+        //std::cout << ' ';
+        std::cout << "|";
+        for(size_t i{0}; i < l.size(); i++){
+            auto ch = l[i];
+            std::cout   << "   " << (ch == 1 ? '|' : ' ' ) ;
+        }
+        std::cout << "|";
+        std::cout << std::endl;
+        std::cout << "|";
+        //std::cout << ' ' <<  std::to_string(std::get<cell_i>(setv[0])) << ' ';
         for(size_t i{0}; i < l.size(); i++)
         {
             auto ch = l[i];
             auto ch2 = l2[i];
+            auto n = (ch2 == 1 ? '_' : ' ');
+            auto n2 = n;
+            if(false) {
 
-            std::cout << (ch == 1 ? '|' : ' ' )<< (ch2 == 1 ? '_' : ' ' );
+                    n = std::to_string(std::get<cell_i>(setv[i+1]))[0];
+                    n2 = std::to_string(std::get<cell_i>(seth[i+1]))[0];
+            }
+            std::string str = {(ch2 == 1 ? "___" : "   ")};
+            str[0] = n;
+            str[2] = n2;
+            std::cout  << str.c_str() << (ch == 1 ? '|' : ' ' ) ;
         }
+        
+         std::cout << "|";
          std::cout << std::endl;
         sleep(1) ;
     }
@@ -77,15 +104,16 @@ void test_maze()
     }
 }
 
-free_list<int> make_free_list()
-{
-    free_list<int> lst;
-    lst.reserve(10);
-    for (size_t i = 0; i < 10; i++){
-        lst.insert(i);
-    }
-    return lst;
-}
+//free_list<int> make_free_list()
+//{
+    //free_list<int> lst;
+    //lst.reserve(10);
+   // for (size_t i = 0; i < 10; i++){
+     //   lst.insert(i);
+  //  }
+    //return lst;
+//}
+/*
 void test_erase()
 {
     auto lst = make_free_list();
@@ -96,16 +124,17 @@ void test_erase()
     auto it = std::ranges::find(lst, 5);
     lst.erase(it);
 }
+*/
 void test_free_list_iterator()
 {
-    auto lst = make_free_list();
+   // auto lst = make_free_list();
     
 
     
     //std::cout <<  *(++lst.begin());
     //std::cout << *(lst.begin() +1);
     
-    for(auto i : lst)
+    //for(auto i : lst)
     {
         //std::cout << i ;
     }
